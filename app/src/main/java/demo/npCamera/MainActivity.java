9e3ce2af44de3563155124448f3c86c92374fee7
nopointer <857508412@qq.com>
2019-09-18 20:08:52 +0800

    初始化相机

--- app/src/main/java/demo/npCamera/MainActivity.java
+++ app/src/main/java/demo/npCamera/MainActivity.java
@@ -18,13 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_SMS,
-                    Manifest.permission.READ_PHONE_STATE,
-                    Manifest.permission.READ_CALL_LOG
-            }, 100);
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
         }
 
 
